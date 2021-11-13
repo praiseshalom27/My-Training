f9db925499fc20a8670133eca7de44a6a3d0be13
praiseshalom27 <89846529+praiseshalom27@users.noreply.github.com>
2021-11-13 07:54:50 +0530

    uploading my project to git reository

---
+++ FirstMaven/src/main/java/excelcode/ExcelMain.java
@@ -0,0 +1,20 @@
+package excelcode;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Excel ob= new Excel();
+		String a=ob.readData(0,0);
+        System.out.println("Value of a is "+a);
+        
+        String b=ob.readData(0,1);
+        System.out.println("Value of b is "+b); 
+		
+		
+
+	}
+
+}
